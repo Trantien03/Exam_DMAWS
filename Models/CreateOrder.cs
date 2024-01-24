62d4d395b141695323b32273cddba837193ae738
tran van tien <trantien03@gmail.com>
2024-01-24 20:14:45 +0700

    exam

---
+++ Models/CreateOrder.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace baithiDMAWS.Models
+{
+    public class CreateOrder
+    {
+        [Required(ErrorMessage = "Please Enter Code")]
+        public string ItemCode { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Name")]
+        public string ItemName { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Quantity")]
+        public int ItemQty { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Delivery Time")]
+        public DateTime OrderDelivery { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Address")]
+        public string OrderAddress { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Phone Number")]
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ Models/EditOrder.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace baithiDMAWS.Models
+{
+    public class EditOrder
+    {
+        [Required(ErrorMessage = "Please Enter Id")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Delivery Time")]
+
+        [DataType(DataType.Date)]
+
+        [DisplayFormat(DataFormatString = "{0:YY-MM-DD}", ApplyFormatInEditMode = true)]
+        public DateTime OrderDelivery { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Address")]
+        public string OrderAddress { get; set; }
+    }
+}
